192ff08f5965f5e5b67a5749c881b2082fb11224
Marah Zidan <marahz@student.uia.no>
2022-04-29 19:55:34 +0200

    Fixed merging conflict

--- patient_monitoring/App.js
+++ patient_monitoring/App.js
@@ -102,3 +102,76 @@ const styles = StyleSheet.create({
 });
 
 export default App;
+
+/*import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import Ionicons from "react-native-vector-icons/Ionicons";
+
+import { createAppContainer, createSwitchNavigator } from "react-navigation";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "react-navigation-stack";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+
+import colors from "./assets/colors/colors";
+
+import Home from "./components/Home";
+import Export from "./components/Export";
+import Profile from "./components/Profile";
+import newPasient from "./components/newPasient";
+import BloodPressure from "./components/BloodPressure";
+import Login from "./components/Login";
+
+
+import FirebaseKeys from "./firebase";
+
+const tabNavigator = createBottomTabNavigator(
+  {
+    Home: {
+    screen: Home,
+    BottomTabNavigationOptions: {
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="home-outline" size={28} color={color} />
+          ),
+      }
+    },
+    Export: {
+      screen: Export,
+      BottomTabNavigationOptions: {
+      options: {
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="download-outline" size={28} color={color} />
+          ),
+      }
+      }
+    },
+    Profile: {
+      screen: Profile,
+      BottomTabNavigationOptions: {
+        tabBarIcon: ({ color }) => (
+          <Ionicons name="person-outline" size={28} color={color} />
+        ),
+      }
+    }
+  },
+  {
+    tabBarOptions: {
+      style: StyleSheet.tabBar,
+      activeTintColor: colors.blue,
+      inactiveTintColor: colors.grey1,
+      showLabel: false,
+    }
+  }
+);
+
+const authStack = createStackNavigator ({
+  Login: Login,
+});
+
+export default tabNavigator (
+  createSwitchNavigator({
+    Home: Home,
+    Profile: Profile
+  }
+  )
+)
+*/
